2cde304c9904458dbbd3b00b831283e096c93a3d
Jnber <benhamidaessia@gmail.com>
2022-01-20 11:36:03 +0100

    added signup

--- dao/auth.py
+++ dao/auth.py
@@ -1,8 +1,4 @@
-import ./dao/UserRepository
+from dao.UserRepository import UserRepository
 from dao.User import User
 
-user: User
-def signup(email, username, password):
-    self.user.email = email
-    self.user.username = username
-    self.user.password = password
+
