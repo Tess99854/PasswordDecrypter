c4c56c4146563dfeb7e813141bc669ac876c56b3
Tess99854 <stesnime@yahoo.com>
2022-01-20 11:27:41 +0100

    add sign in frame

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 
+import db
+from dao.UserRepository import UserRepository
 from frames.main_frame import set_main_frame
-from frames.sign_up_frame import set_sign_up_frame
 
 if __name__ == '__main__':
     root = tk.Tk()
@@ -10,6 +11,8 @@ if __name__ == '__main__':
     canvas = tk.Canvas(root, width=600, height=300, bg='#020426')
     canvas.grid(rowspan=4)
 
-    set_main_frame(root)
-    # set_sign_up_frame(root)
+    user_repository = UserRepository(db.connect())
+
+    set_main_frame(root, user_repository)
+
     root.mainloop()
